505c346283a0d13f8c74a93b214210894b31077f
ahmed alshmre <ahmedalshmre06@outlook.com>
2020-12-16 23:05:54 +0300

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'df0f741489876a1127355969e4dda65c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '13cd9c05-aa03-45cb-8dc8-0185fb393256'
+};
